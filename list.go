f900878f5c623fcdc4cec4a182b08939d6d468d5
iltiaaa <califano05@gmail.com>
2021-11-01 18:32:20 +0100

    first commit

---
+++ list.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+    "os"
+    "fmt"
+    "log"
+    "io/ioutil"
+)
+
+const(
+    colorReset = "\033[0m"
+    colorBlue = "\033[34m"
+    max_arg = 1
+    max_line = 3
+    max_len = 17
+)
+
+func main() {
+
+    var dir = "." 
+    var line = 0
+    var j int
+    var narg = len(os.Args[1:])
+    log.SetFlags(0)
+
+    if narg > max_arg {
+        log.Fatal("Too many arguments")
+    }
+    if narg == 1 {
+        dir = os.Args[1]
+    }
+
+    files, err := ioutil.ReadDir(dir)
+    if err != nil {
+        log.Fatalf("Error no such directory: %v", err)
+    }
+
+    for _, file := range files {
+        fname := file.Name()
+        if line == max_line {
+            line = 0
+	    fmt.Printf("\n")
+        }
+        if string(fname[0:1]) != "." {
+            cdir, err := os.Stat(fname)
+            if err != nil {
+		log.Fatal(err)
+	    }
+
+            if cdir.IsDir() {
+		fmt.Printf(string(colorBlue))
+		fmt.Printf(fname)
+                for j=0; j<(max_len-len(fname)); j++ {
+		    fmt.Printf(" ")
+		}
+		fmt.Printf(string(colorReset))
+	    }else {
+                fmt.Printf(fname)
+		for j=0; j<(max_len-len(fname)); j++ {
+		    fmt.Printf(" ")
+                }
+            }
+        line++
+        }
+    }
+    fmt.Printf("\n")
+}
